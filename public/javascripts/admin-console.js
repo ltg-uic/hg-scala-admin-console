4dc354e5ff302ef4587792a7e1c8b51252f7edd0
Alessandro Gnoli <tebemis@gmail.com>
2013-10-29 16:23:32 -0500

    Added support for simulator commands and fixed the nav menu

--- public/javascripts/admin-console.js
+++ public/javascripts/admin-console.js
@@ -5,16 +5,29 @@
 // Represents the current run_id
 var run_id;
 
+// Simulator variables
+var departure;
+var arrival;
+var tag;
 
 // Executed when the DOM is fully loaded using JQuery.ready
 $(	function() {
 	
 	// Adds a listener to the drop down bout menu
-	$('.dropdown-menu a').click( function () {
+	$('#run_id_dropdown a').click( function () {
 		if ($(this).text().toLowerCase() != run_id)
 			$('#debug_console').empty()
 	    run_id = $(this).text().toLowerCase()
-		$('#current_run').text($(this).text())
+		$('#run_id_a').html($(this).text()+'<b class="caret"></b>')
+	});
+	// Adds a listener to the departure and arrivals dropdowns
+	$('#departure_dropdown a').click( function () {
+	    departure = $(this).text()
+		$('#departure_btn').html( $(this).text() + ' <span class="caret"></span>')
+	});
+	$('#arrival_dropdown a').click( function () {
+	    arrival = $(this).text()
+		$('#arrival_btn').html( $(this).text() + ' <span class="caret"></span>')
 	});
 	
 	// Setup callbacks for the command buttons
@@ -39,6 +52,14 @@ $(	function() {
 			var current_bout = $('#bout_id').val()
 			if (typeof current_habitat !== "undefined" && typeof current_bout !== "undefined")
 			sendMessage({ "run_id": run_id, "ltg_event": { "event": "reset_bout", "payload": {  "habitat_configuration_id": current_habitat, "bout_id": current_bout  } } })
+	});
+	$("#move_btn").click(function() {
+		tag =  $('#tag_id').val()
+		if (typeof run_id !== "undefined" && typeof departure !== "undefined" && typeof arrival !== "undefined" && typeof tag !== "undefined") {
+			if (departure != arrival) {
+				sendMessage({ "run_id": run_id, "ltg_event": { "event": "rfid_update", "payload": { "id": tag, "departure": departure, "arrival": arrival } } })
+			}
+		}
 	});
 
 	// Connect Websockets
